12a5b6842fd01cb42821d8ec34dd91ecf1020e18
MaksimSkavysh <maksim.skavysh@gmail.com>
2016-11-02 13:37:49 +0300

    adding page lections

--- app/pages/lessons/lessons.component.js
+++ app/pages/lessons/lessons.component.js
@@ -9,20 +9,54 @@ component('lessons', {
         function diplomsController(ModalService, VarsService, $scope) {
             var self = this;
             this.courses = vars.courses;
+
             this.activeCourse = this.courses[0] ? 0 : undefined;
+
+            this.newGroup = {name: undefined, all: undefined, mou: undefined};
+            this.hours=[
+                { name:'ткз', value: 1 },
+                { name:'к.р.', value: 1 },
+                { name:'зачет', value: 1 },
+                { name:'экзамен', value: 1 },
+                { name:'консультация', value: 1 },
+                { name:'текю контроль', value: 1 }
+            ];
+
+            this.addGroup = function(){
+                if (!this.newGroup.name || !this.newGroup.all) {
+                    alert('Неполные данные');
+                    return
+                }
+                this.courses[this.activeCourse].groups.push(VarsService.createEntityWithId({
+                    name: this.newGroup.name,
+                    all: this.newGroup.all,
+                    mou: this.newGroup.mou !== undefined ? this.newGroup.mou : 0
+                }));
+                for (var key in this.newGroup) {
+                    this.newGroup[key] = undefined;
+                }
+                VarsService.saveVars(vars);
+            };
+
+            this.removeGroup = function (index) {
+                this.courses[this.activeCourse].groups.splice(index, 1);
+                VarsService.saveVars(vars);
+            };
+
 
             this.addCourse = function(){
                 ModalService.showModal({
                     templateUrl: "components/tpl/add.course.tpl.html",
                     controller: ["courses", "close", function RatesModifyingController(courses, close) {
                         this.courses = courses;
+
                         this.newCourse;
                         this.close = function () {
                             close(null, 500);
                         };
                         this.addNewCourses = function () {
                             if(this.newCourse){
-                                this.courses.push(VarsService.createEntityWithId({name:this.newCourse}));
+                                this.courses.push(VarsService.createEntityWithId({name:this.newCourse, groups:[]}));
                                 this.newCourse = undefined;
                             }
                         };
